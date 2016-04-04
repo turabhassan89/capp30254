b93f89e53a5d0fbb1c13fbf06eb0da0d7e47201a
Turab Hassan <turabhassan89@gmail.com>
2016-04-04 16:45:20 -0500

    safety submission

---
+++ PA 1/pa1.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import urllib.request
+import json
+import matplotlib.pyplot as plt
+
+
+genderize = 'https://api.genderize.io/?name='
+
+def pa1():
+    df = pd.read_csv('mock_student_data.csv')
+    
+    #Plotting the Histograms
+    df.hist('Age')
+    plt.savefig('Age')
+    df.hist('GPA')
+    plt.savefig('GPA')
+    df.hist('Days_missed')
+    plt.savefig('Days_missed')    
+    
+    #Finding the Summary Stats
+    print( df.describe ( percentiles = [] ).transpose() )
+    print( df.mode(numeric_only = True ) )
+    print (df.isnull().sum())
+    
+    #Filling in the Missing values for Gender
+    for index, row in df.iterrows():
+        if pd.isnull( row['Gender'] ):
+            name = row['First_name']
+            url = genderize + name
+            request = urllib.request.urlopen(url)
+            data = json.loads(request.read().decode('utf8'))
+            gender = data['gender'].capitalize()
+            df.set_value(index,'Gender', gender) 
+    
+    #Filling in Missing Values Based on mean   
+    df_mean = df.copy (deep = True)
+    
+    mean_age = df_mean['Age'].mean()
+    df_mean['Age'].fillna ( mean_age, inplace = True )
+
+    mean_gpa = df_mean['GPA'].mean()
+    df_mean['GPA'].fillna ( mean_gpa, inplace = True )
+
+    mean_daysmissed = df_mean['Days_missed'].mean()
+    df_mean['Days_missed'].fillna ( mean_daysmissed, inplace = True )
+
+
+    df_mean.to_csv('mean.csv')
+
+
+    #Filling in missing values based on conditional mean
+    df_conditional_mean = df.copy(deep = True)
+
+    mean_age_grad_yes = df_conditional_mean['Age'][ df_conditional_mean['Graduated'] == 'Yes' ].mean()
+    mean_age_grad_no = df_conditional_mean['Age'][ df_conditional_mean['Graduated'] == 'No' ].mean()
+    
+    mean_gpa_grad_yes = df_conditional_mean['GPA'][ df_conditional_mean['Graduated'] == 'Yes' ].mean()
+    mean_gpa_grad_no = df_conditional_mean['GPA'][ df_conditional_mean['Graduated'] == 'No' ].mean()
+    
+    mean_daysmissed_grad_yes = df_conditional_mean['Days_missed'][ df_conditional_mean['Graduated'] == 'Yes' ].mean()
+    mean_daysmissed_grad_no = df_conditional_mean['Days_missed'][ df_conditional_mean['Graduated'] == 'No' ].mean()
+
+    for index, row in df_conditional_mean.iterrows():
+        if pd.isnull(row['Age']) and row['Graduated'] == 'Yes':
+            df_conditional_mean.set_value(index,'Age', mean_age_grad_yes)
+        if pd.isnull(row['Age']) and row['Graduated'] == 'No':
+            df_conditional_mean.set_value(index,'Age', mean_age_grad_no)
+
+        if pd.isnull(row['GPA']) and row['Graduated'] == 'Yes':
+            df_conditional_mean.set_value(index,'GPA', mean_gpa_grad_yes)
+        if pd.isnull(row['GPA']) and row['Graduated'] == 'No':
+            df_conditional_mean.set_value(index,'Age', mean_gpa_grad_no)
+        
+        if pd.isnull(row['Days_missed']) and row['Graduated'] == 'Yes':
+            df_conditional_mean.set_value(index,'Days_missed', mean_daysmissed_grad_yes)
+        if pd.isnull(row['Days_missed']) and row['Graduated'] == 'No':
+            df_conditional_mean.set_value(index,'Days_missed', mean_daysmissed_grad_no)
+
+    df_conditional_mean.to_csv('conditional_mean.csv')
+
+    #Another way to filling in the missing values is to condition on two things; Gender and Graduation
+    #Find the means for Male Graduated, Male Not Graduated, Female Graduated and Female Not Graduated
+    #and fill in the missing values accordingly.        
+            
+    return None
